d3699ab86f36ee159c7c7444c0e88f43d53252bf
Vishvajeet31 <155959029+Vishvajeet31@users.noreply.github.com>
2024-03-18 14:04:48 +0530

    Create String & Conditional.py

---
+++ Python/String & Conditional.py
@@ -0,0 +1,122 @@
+# Strings & Conditonals
+
+# str1 = "apna "
+# str2 = "college"
+# print(str1+" "+str2)
+# # a = str1 + "  " + str2
+# print(len(str1))
+# # print(a)
+# print(str1[2])
+# print(str1[0:2])
+# print(str1[-3:-1])
+
+# str = "i am learning python from apna college."
+# # print(str.endswith("am"))
+# # print(str.capitalize())
+# print(str.replace("python","java"))
+# # print(str.find("p"))
+# print(str.count("a"))
+
+# Quesstion 1:
+
+# name = input("Enter the name here ")
+# print (len(name))
+
+# Question 2:
+
+# a = "i will pade you $100 and you have to return me $120"
+# print(a.count("$"))
+
+# CONDITIONAL STATEMENTS
+
+# age = 2
+# if(age>=18):
+    # print("you are eligable for voting")
+# elif(age<18):
+    # print("You are not eligable for voting")
+# else:
+    # print("You are not eligable for voting")
+
+# Question : Grade student based on marks. 
+
+# Marks = int(input("Enter your marks here : "))
+
+# if (Marks >= 90):
+#     print("grade = A")
+# elif(90 > Marks >= 80):
+#     print("grade = B")
+# elif(80 > Marks >= 70):
+#     print("grade = C")
+# else:
+#     print("grade = D")  
+
+# Question : 2
+
+# num = int(input("Enter the number here : "))
+# rem = num % 2
+# if(rem == 0 ):
+#     print("even")
+# else:
+#     print("odd")
+
+# Question : 3
+
+# a = int(input("Enter the number here : "))
+# b = int(input("Enter the second number here : "))
+# c = int(input("Enter the third number here: "))
+# if (a>b and a>c):
+#     print("a is greater than rest of two.")
+# elif(b>a and b>c):
+#     print("b is greater than rest of two.")
+# else:
+#     print("c is  greater than rest of the two. ")
+
+# Question : 4
+# a = int(input("Enter the number here  : "))
+
+# if (a % 7 == 0):
+#     print("multiple of 7")
+# else:
+#     print("not multiple of 7")
+
+# Question :
+# age = int(input("Enter the age here : "))
+# if age <=13:
+#     print("You are a child")
+# elif (age>=14 and age<=19):
+#     print("You are Teenager")
+# elif (age>=20 and age<=59):
+#     print("Adult")
+# else:
+#     print("Senior")
+
+# Question :
+# Ticket_price = 350
+# Day = input("Enter the day here : ")
+# age = int(input("Enter the age here :"))
+# if age < 18 and age>10:
+#    Ticket_price -= 50
+#    print("you are a adult, your ticket price is",Ticket_price)
+# elif age < 10:
+#    Ticket_price -= 100
+#    print("you are a , your ticket price is  ",Ticket_price)
+# elif (Day == "Wednesday"):
+#    Ticket_price -= 12
+#    print("Thankyou",Ticket_price)
+
+# Question :
+# score = 55
+# if (score >= 90 and score <= 100):
+#     print("A grade")
+# elif (score >=80 and score <= 89):
+#     print("B grade")
+# elif (score >= 70 and score <= 79):
+#     print("C grade") 
+# elif (score >= 60 and score <= 69):
+#     print("D grade")
+# else:
+#     print("Fail")
+
+# THENKYOU
+
+
