8187bd6c1032151706cf94d5819307945f3d8678
Vishvajeet31 <155959029+Vishvajeet31@users.noreply.github.com>
2024-03-20 23:46:52 +0530

    Add files via upload

---
+++ Python/Dictionary & Set.py
@@ -0,0 +1,38 @@
+# info = {"name":"vishu","CGPA":8,"marks":[55,77,33],"topics":("python","java")}
+square_num = {x:x**2 for x in range(5)}
+print(square_num)
+# # info["name"] = "rathi"
+# # # print(type(info))
+# # info["CGPA"] = 9
+# print(info.keys())
+# print(info.values())
+# print(info.items())
+# print(info.get("name"))
+# info.update({"city":"delhi"})
+# print(info)
+# # null_dict = {}
+# # null_dict["place"] = "Dubai"
+# print(null_dict)
+# student = {"name":"vishu","subject":{"phy":55,"chem":44,"eng":22}}
+# student["subject"] = {"hindi":44}
+# print(student["name"])
+# print(student)
+
+# SET
+# set = set()  
+# set.add(1)
+# set.add("vishu")
+# print(type(set))
+# print(len(set))
+# print(set)
+
+# set1 = {1,2,3}
+# set2 = {3,4,5}
+# print(set1.union(set2))
+# # print(set1.intersection(set2))
+# print(type(set1))
+
+# Quesstion : 1
+# "dict = {}
+# dict["a piece of furniture"] = "chair","table"
+# print(dict)"
