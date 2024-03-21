9bab0e08d5b1a9d16b8926e03794e4b68e8cc962
Vishvajeet31 <155959029+Vishvajeet31@users.noreply.github.com>
2024-03-21 22:59:49 +0530

    Add files via upload

---
+++ Python/Files.py
@@ -0,0 +1,40 @@
+# READ :
+# f = open("Demo.txt","r")
+# # data = f.read()
+# data = f.readline()
+# data2 = f.readline()
+
+# print(data2)
+
+# f.close()
+
+# WRITE :
+# f = open ("Demo.txt","a")
+# f.write("This is new addition\nHi right now i am appending this file")
+# f.close
+
+# f = open("Sample.txt","w")
+# f.close()
+
+# WITH syntax :
+# with open("Demo.txt","r")as f:
+#     data = f.read()
+#     print(data)
+
+# with open("sample.txt","w")as f:
+#     f.write()
+# f = open("sample.txt","w")
+# f.close
+
+# import os 
+# os.remove("sample.txt")
+
+# Question 1 :
+# f = open("practice.txt","w")
+# f.write("HI  everyone\nwe are learning Files \nusing python.")
+# # f.find
+# f.close()
+
+# Question 2 :
+import os 
+os.remove("practice.txt")
