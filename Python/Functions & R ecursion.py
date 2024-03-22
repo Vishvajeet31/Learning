f2c4310f7146a7f2d1b857248537cc88a048cbf2
Vishvajeet31 <155959029+Vishvajeet31@users.noreply.github.com>
2024-03-22 21:31:03 +0530

    Add files via upload
    new change

---
+++ Python/Functions & R ecursion.py
@@ -0,0 +1,90 @@
+# def cal_sum (a,b):
+#     return a + b
+# print(cal_sum(1,2))
+
+# def hellow ():
+#     print("hellow world")
+
+# hellow()
+
+# def cal_avg (a,b,c):
+#     sum = a+b+c
+#     average = sum/3
+#     return average
+# print(cal_avg(1,2,3))
+
+# Question : 1
+# cities = ["delhi","roorkee","haridwar"]
+# heroes = ["thor","ironman","caption america"]
+
+# def print_list(list):
+#     print(len(list))
+# print_list(cities)
+# print_list(heroes)
+
+# Question : 2
+# def print_f (n):
+#     fact = 1 
+#     for i in range(1, n+1):
+#         fact *= i
+#     print(fact)
+
+# print_f(5)
+
+# Question : 3
+# def converter(usd_val):
+#     inr_val = usd_val * 83
+#     print(usd_val,"USD = ", inr_val,"INR")
+
+# converter(100)
+
+# Question : 5
+# def find(n):
+#     if (n%2==0):
+#         print("even")
+#     else:
+#         print("odd")
+
+# find(2)
+
+# Question :
+# def squre(num):
+#     return num**2
+# result = squre(4)
+# print(result)
+
+# Question :
+# def sum(a,b,c):
+#     return a+b+c
+# result = sum(1,2,3)
+# print(result)
+
+# Question :
+# import math
+# def circle(radius):
+#     area =  math.pi*radius*radius
+#     circumfrence = 2*math.pi*radius
+#     return area,circumfrence
+# print(circle(3))
+
+# # Question : 
+# def greet (name):
+#     return "hellow, " + name + "!"
+# print(greet("vishu"))
+
+# Question :
+def cube (num):
+    return num ** 3
+print(3)
+# or 
+cube = lambda x: x**3
+print(cube(3))
+
+# RECURSION
+
+
+
+
+
+
+
