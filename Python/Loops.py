324c4f67234570f18f15c8fb2a80a66804ce9adf
Vishvajeet31 <155959029+Vishvajeet31@users.noreply.github.com>
2024-03-23 21:53:04 +0530

    Add files via upload

---
+++ Python/Loops.py
@@ -0,0 +1,142 @@
+# While Loop
+
+# i = 1
+# while i<=5:
+#     print("hellow world",i)
+#     i += 1
+
+# i = 5 
+# while i >= 1:
+#     print(i)
+#     i -= 1
+
+# #  question : 1
+# i = 1
+# while i<=100:
+#     print(i)
+#     i += 1
+# print ("loop end")
+
+# Question : 2
+# i = 100
+# while i>=1:
+#     print(i)
+#     i -= 1
+
+# Question : 3
+# num = int(input("Enter the number here : "))
+# i = 1
+# while i <= 10:
+#     print(num,"*",i, "=", num * i )
+#     i += 1
+
+# Question = 4
+# num = [1,4,9,16,25,36,49,64,81,100]
+
+# idx = 0
+# while idx < len(num):
+#    print(num[idx])
+#    idx += 1  
+
+# Quesdtion : 5
+# T = (1,2,3,45,6,78,5)
+# x = 3
+# i = 0
+# while i < len(T):
+#     if (T[i] == x):
+#         print("found at index ",i)
+#     else:
+#         print("finding..")
+#     i += 1
+
+# Break
+# i = 1
+# while i<= 5:
+#     print(i)
+#     if (i==3):
+#         break
+#     i += 1
+
+# Contineous
+# i = 1
+# while i<=5:
+#     if (i == 3):
+#         i += 1
+#         continue
+#     print(i)
+#     i += 1   
+
+# For Loop
+# list= [1 ,2 ,3 ,4 ,5]
+
+# for el in list:
+#     print(el)
+
+# tup = (1,2,3,4,5,6,7)
+
+# for el in tup:
+#     print(el)  
+
+# Question : 1
+# list = [1,23,45,67,8,543,2,4]
+# x = 45
+# for el in list:
+#     if (x == el):
+#         break
+#     print(el)
+# print("thankyou for using loop")
+
+# Range
+# seq = range(5)
+# # print(seq)
+# # print(seq[1])
+# for i in seq:
+#     print(i)
+# for i in range(2,10,2):
+    # print(i)
+# for i in range(1,100,2):
+    # print(i)
+# for i in range(1,11):
+#     print(5,"*",i,"=", 5*i)
+
+# Pass
+# for i  in range(5):
+#     pass 
+# print ("work done")
+
+# Question : 1
+# for i in range(5):
+#     print(i+1)
+
+# Question :
+# number = [1,-2,3,-4,5,-6,7,-8,9]
+# positive_number_count = 0
+# for num in number:
+#     if num>0:
+#          positive_number_count += 1
+# print(positive_number_count)
+
+# Question :
+
+# n = 10 
+# sum_even = 0
+# for i in range(1,n+1):
+#     if i%2 == 0:
+#         sum_even += 1
+# print(sum_even)
+
+# Question :
+
+# n = 10
+# for i in range(1,n+1):
+#     if i == 5:
+#         break
+#     print(n,"*",i,"=",n*i)
+
+# Question :
+# str = "Harry"
+# for i in str:
+#     print(i)
+
+
+      
