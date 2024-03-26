65e8afb4dee18d6aef3c65331047660e68dbe564
Vishvajeet31 <155959029+Vishvajeet31@users.noreply.github.com>
2024-03-26 21:57:06 +0530

    Add files via upload

---
+++ Python/List & TUple.py
@@ -0,0 +1,59 @@
+# marks = [5,6,7,8,3,"vishu"]
+# print(marks)
+# print(type(marks))
+# print(marks[1])
+# print(len(marks))
+# marks[-1] = "Rathi"
+# print(marks).
+# print(marks[1:])
+# marks.sort()
+# print(marks)
+# marks.append("rathi")
+# print(marks)
+# marks = [2,4,3,5,6]
+# marks.sort(reverse = True)
+# print(marks)
+chai = ["mint tea","leamon tea","normal tea"]
+chai[1:2] = ["tea"]
+print(chai)
+# print(chai[:2])
+
+# TUPLE
+# tup = (1,2,3,4,5)
+# print(tup[0:3])
+
+# tup = ()
+# print(tup) 
+# print(type(tup))
+
+# tup = (1,)
+# print(tup)
+
+# Questtion : 1
+# movie = []
+# movie1 = input("Enter the movie here : ")
+# movie2 = input("Enter the movie here : ")
+# movie3 = input("Enter the movie here : ")
+# # movie  = movie1+" " +movie2+" " +movie3
+# print(movie.append(movie1))
+# print(movie.append(movie2))
+# print(movie.append(movie3))
+# print(movie)
+
+# # Question : 2
+# # L = [1,2,3,2,1]
+# # copy_L = L.copy()
+# # copy_L.reverse()
+# # if (copy_L == L):
+# #     print("palindrome")
+# # else:
+# #     print("not palindrone")
+
+# # Question : 3
+# # L = ["F","D","E","A","W"]
+# L.count("A")
+# print(L)
+# L.sort()
+# print(L)    
+
+
