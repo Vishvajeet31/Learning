8cf099f347cf269f5eecc8520a24c8460d192bab
Vishvajeet31 <155959029+Vishvajeet31@users.noreply.github.com>
2024-03-27 20:49:39 +0530

    Add files via upload

---
+++ Python/Operator & Input.py
@@ -0,0 +1,78 @@
+# print("Hellow world")
+# print(5)
+# print(5+5)
+# a = 5
+# b = 5
+# print("The sum of a+b is :", a+b)
+
+# INPUT FUNCTION
+# old_age = input("Enter your old age.")
+# new_age = int(old_age) + 2
+# print("Your new age is :" , new_age)
+
+# STRING CHANGE
+# name = "Tony stark"
+# print(name.upper())
+# print(name.find('o'))
+# print(type(name))
+
+#ASSIGNMENT OPERATOR
+# print(5**4)
+# i = 5
+# i += 2
+# i -= 2
+# i /= 2
+# i //= 2
+# print(i)
+
+# # RELATION OPERATOR
+# a = 50 
+# b = 40
+# print(a==b)
+# print(a!=b)
+# print(a>=b)
+# print(a<=b)
+
+# LOGICAL OPERATORS
+# print(5>6 or 6<7)
+# print(5>6 and 6<7)
+# print(not 5>6)
+
+# TYPE CASTING
+# a = 1
+# b = '2'
+# print(a + int(b))
+
+# INPUT
+# name = input("Enter your name here: ")
+# print("welcome",name)
+
+# Let's practice
+# Question : 1
+# a = int(input("Enter the number here:"))
+# b = int(input("Enter the second number here:"))
+# sum = a+b
+# print("The sum of two number is =",sum)
+
+
+# Question : 2
+# side = float(input("Enter the side of squre here : "))
+# Area = side*side
+# print ("Area of squre is :",Area)
+
+# Question : 3
+# a = float(input("Enter the first value here:"))
+# b = float(input("Enter the second value here:"))
+# average = a+b/2
+# print("The average of two number is :",average)
+
+# Question : 4
+# a = int(input("Enter the number here:"))
+# b = int(input("Enter the anothe number here:"))
+# print(a>=b)
+
+
+# a = True +4
+# print(a)
+
+# THANKYOU
