285b3ebc8598e2e728c4894dc1302b6fbf9ac24e
Vishvajeet31 <155959029+Vishvajeet31@users.noreply.github.com>
2024-04-01 20:51:23 +0530

    Add files via upload

---
+++ Time Module.py
@@ -0,0 +1,19 @@
+# import time
+
+# def usingWhile():
+#     i = 0
+#     while i<50000:
+#         print(i)
+#         i = i+1
+# def usingfor():
+#     for i in range(50000):
+#         print(i)
+
+# init = time.time()
+# # usingWhile()
+# usingfor()
+# print(time.time()-init)
+
+# print(5)
+# time.sleep(3)
+# print("It will print after 2 second")
