ce6c89068f90bab2b9cd2b6f089ca0744b84eba7
Vishvajeet31 <155959029+Vishvajeet31@users.noreply.github.com>
2024-07-08 14:31:29 +0530

    Add files via upload

--- Web Development/script.js
+++ Web Development/script.js
@@ -1,1 +1,33 @@
-alert("Welcome to sigma world.")
+// let a = 10;
+// let b = 5;
+// // let output = "The total price is :"+(pencilPrice + erasorPrice) +  " Rupees.";
+// // console.log(output);
+// console.log(`Your pay ${a+b} rupees`);
+// let color = "r";
+// if (color === "red"){
+//     console.log("stop");
+// }else if (color === "yellow"){
+//     console.log("slow down");
+// }else if (color === "green"){
+//     console.log("go");
+// }else{
+//     console.log("traffic light is broken");
+// }
+// let name ="vishvajeet rathi"
+// console.log(name)
+// let color = "Green";
+// switch(color){
+//     case "Red":
+//         console.log("stop");
+//         break;
+//     case "Green":
+//         console.log("go");
+//         break;
+//     default:
+//         console.log("broken light");   
+// }
+// alert("smt going wrong");
+// prompt("enter your rollno."
+// console.error("there is error");
+let students = ["aman","shradha"];
+console.log(students);    
